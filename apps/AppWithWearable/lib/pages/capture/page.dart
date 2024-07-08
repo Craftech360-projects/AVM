5d865a7009bdfa93a7de236c773d2d6e84adfab5
rahulpscraftech360 <rahul@craftech360.com>
2024-07-08 18:04:32 +0530

    added animation movigright to left

--- apps/AppWithWearable/lib/pages/capture/page.dart
+++ apps/AppWithWearable/lib/pages/capture/page.dart
@@ -1,18 +1,192 @@
+// // import 'package:flutter/material.dart';
+// // import 'package:AVMe/backend/mixpanel.dart';
+// // import 'package:AVMe/backend/preferences.dart';
+// // import 'package:AVMe/backend/schema/bt_device.dart';
+// // import 'package:AVMe/pages/speaker_id/page.dart';
+// // import 'package:AVMe/widgets/device_widget.dart';
+// // import 'package:AVMe/widgets/scanning_ui.dart';
+
+// // import 'widgets/transcript.dart';
+
+// // class CapturePage extends StatefulWidget {
+// //   final Function refreshMemories;
+// //   final BTDeviceStruct? device;
+
+// //   // final int batteryLevel;
+// //   final GlobalKey<TranscriptWidgetState> transcriptChildWidgetKey;
+
+// //   const CapturePage({
+// //     super.key,
+// //     required this.device,
+// //     required this.refreshMemories,
+// //     required this.transcriptChildWidgetKey,
+// //   });
+
+// //   @override
+// //   State<CapturePage> createState() => _CapturePageState();
+// // }
+
+// // class _CapturePageState extends State<CapturePage>
+// //     with AutomaticKeepAliveClientMixin {
+// //   bool _hasTranscripts = false;
+
+// //   @override
+// //   bool get wantKeepAlive => true;
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return ListView(children: [
+// //       SharedPreferencesUtil().hasSpeakerProfile
+// //           ? const SizedBox(height: 16)
+// //           : Stack(
+// //               children: [
+// //                 GestureDetector(
+// //                   onTap: () {
+// //                     Navigator.of(context).push(MaterialPageRoute(
+// //                         builder: (c) => const SpeakerIdPage()));
+// //                     MixpanelManager().speechProfileCapturePageClicked();
+// //                   },
+// //                   child: Container(
+// //                     decoration: BoxDecoration(
+// //                       color: Colors.grey.shade900,
+// //                       borderRadius: const BorderRadius.all(Radius.circular(12)),
+// //                     ),
+// //                     margin: const EdgeInsets.symmetric(
+// //                         vertical: 16, horizontal: 24),
+// //                     padding: const EdgeInsets.all(16),
+// //                     child: const Row(
+// //                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// //                       children: [
+// //                         Expanded(
+// //                           child: Row(
+// //                             children: [
+// //                               Icon(Icons.multitrack_audio),
+// //                               SizedBox(width: 16),
+// //                               Text(
+// //                                 'Set up speech profile',
+// //                                 style: TextStyle(
+// //                                     color: Colors.white, fontSize: 16),
+// //                               ),
+// //                             ],
+// //                           ),
+// //                         ),
+// //                         Icon(Icons.arrow_forward_ios)
+// //                       ],
+// //                     ),
+// //                   ),
+// //                 ),
+// //                 Positioned(
+// //                   top: 12,
+// //                   right: 24,
+// //                   child: Container(
+// //                     width: 12,
+// //                     height: 12,
+// //                     decoration: const BoxDecoration(
+// //                         color: Colors.red, shape: BoxShape.circle),
+// //                   ),
+// //                 ),
+// //               ],
+// //             ),
+// //       ..._getConnectedDeviceWidgets(),
+// //       TranscriptWidget(
+// //           btDevice: widget.device,
+// //           key: widget.transcriptChildWidgetKey,
+// //           refreshMemories: widget.refreshMemories,
+// //           setHasTranscripts: (hasTranscripts) {
+// //             if (_hasTranscripts == hasTranscripts) return;
+// //             setState(() {
+// //               _hasTranscripts = hasTranscripts;
+// //             });
+// //           }),
+// //       const SizedBox(height: 16)
+// //     ]);
+// //   }
+
+// //   _getConnectedDeviceWidgets() {
+// //     if (_hasTranscripts) return [];
+// //     if (widget.device == null) {
+// //       return [
+// //         const DeviceAnimationWidget(
+// //           sizeMultiplier: 0.7,
+// //         ),
+// //         const ScanningUI(
+// //             string1: 'Looking for AVMe wearable',
+// //             string2: 'Locating your AVMe device.',
+// //             string3: 'Keep it near your phone for pairing'),
+// //       ];
+// //     }
+// //     // return [const DeviceAnimationWidget()];
+// //     return [
+// //       const Center(child: DeviceAnimationWidget(sizeMultiplier: 0.7)),
+// //       Center(
+// //           child: Row(
+// //         crossAxisAlignment: CrossAxisAlignment.center,
+// //         mainAxisAlignment: MainAxisAlignment.center,
+// //         children: [
+// //           const SizedBox(width: 24),
+// //           Column(
+// //             crossAxisAlignment: CrossAxisAlignment.start,
+// //             mainAxisAlignment: MainAxisAlignment.start,
+// //             children: [
+// //               const Text(
+// //                 'Listening',
+// //                 style: TextStyle(
+// //                     fontFamily: 'SF Pro Display',
+// //                     color: Colors.white,
+// //                     fontSize: 29.0,
+// //                     letterSpacing: 0.0,
+// //                     fontWeight: FontWeight.w700,
+// //                     height: 1.2),
+// //                 textAlign: TextAlign.center,
+// //               ),
+// //               Text(
+// //                 'DEVICE-${widget.device?.id.split('-').last.substring(0, 6)}',
+// //                 style: const TextStyle(
+// //                   color: Colors.white,
+// //                   fontSize: 16.0,
+// //                   fontWeight: FontWeight.w500,
+// //                   height: 1.5,
+// //                 ),
+// //                 textAlign: TextAlign.center,
+// //               )
+// //             ],
+// //           ),
+// //           const SizedBox(width: 24),
+// //           Container(
+// //             width: 10,
+// //             height: 10,
+// //             decoration: const BoxDecoration(
+// //               color: Color.fromARGB(255, 0, 255, 8),
+// //               shape: BoxShape.circle,
+// //             ),
+// //           ),
+// //         ],
+// //       )),
+// //       const SizedBox(height: 8),
+// //       const Row(
+// //         crossAxisAlignment: CrossAxisAlignment.center,
+// //         mainAxisAlignment: MainAxisAlignment.center,
+// //         children: [],
+// //       ),
+// //     ];
+// //   }
+// // }
+
+// import 'package:AVMe/widgets/custom_scaffold.dart';
+// import 'package:AVMe/widgets/sin_wave.dart';
 // import 'package:flutter/material.dart';
 // import 'package:AVMe/backend/mixpanel.dart';
 // import 'package:AVMe/backend/preferences.dart';
 // import 'package:AVMe/backend/schema/bt_device.dart';
 // import 'package:AVMe/pages/speaker_id/page.dart';
 // import 'package:AVMe/widgets/device_widget.dart';
+// import 'package:AVMe/widgets/scanning_widget.dart';
 // import 'package:AVMe/widgets/scanning_ui.dart';
-
 // import 'widgets/transcript.dart';
 
 // class CapturePage extends StatefulWidget {
 //   final Function refreshMemories;
 //   final BTDeviceStruct? device;
-
-//   // final int batteryLevel;
 //   final GlobalKey<TranscriptWidgetState> transcriptChildWidgetKey;
 
 //   const CapturePage({
@@ -35,78 +209,90 @@
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return ListView(children: [
-//       SharedPreferencesUtil().hasSpeakerProfile
-//           ? const SizedBox(height: 16)
-//           : Stack(
-//               children: [
-//                 GestureDetector(
-//                   onTap: () {
-//                     Navigator.of(context).push(MaterialPageRoute(
-//                         builder: (c) => const SpeakerIdPage()));
-//                     MixpanelManager().speechProfileCapturePageClicked();
-//                   },
-//                   child: Container(
-//                     decoration: BoxDecoration(
-//                       color: Colors.grey.shade900,
-//                       borderRadius: const BorderRadius.all(Radius.circular(12)),
-//                     ),
-//                     margin: const EdgeInsets.symmetric(
-//                         vertical: 16, horizontal: 24),
-//                     padding: const EdgeInsets.all(16),
-//                     child: const Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: [
-//                         Expanded(
-//                           child: Row(
-//                             children: [
-//                               Icon(Icons.multitrack_audio),
-//                               SizedBox(width: 16),
-//                               Text(
-//                                 'Set up speech profile',
-//                                 style: TextStyle(
-//                                     color: Colors.white, fontSize: 16),
-//                               ),
+//     return CustomScaffold(
+//       body: ListView(
+//         children: [
+//           SharedPreferencesUtil().hasSpeakerProfile
+//               ? const SizedBox(height: 16)
+//               : Stack(
+//                   children: [
+//                     GestureDetector(
+//                       onTap: () {
+//                         Navigator.of(context).push(MaterialPageRoute(
+//                             builder: (c) => const SpeakerIdPage()));
+//                         MixpanelManager().speechProfileCapturePageClicked();
+//                       },
+//                       child: Container(
+//                         decoration: const BoxDecoration(
+//                           gradient: LinearGradient(
+//                             colors: [
+//                               Color.fromARGB(120, 130, 129, 131),
+//                               Color.fromARGB(120, 71, 71, 72),
+//                               Color.fromARGB(122, 49, 11, 125),
 //                             ],
+//                             begin: Alignment.topLeft,
+//                             end: Alignment.bottomRight,
 //                           ),
+//                           borderRadius: BorderRadius.all(Radius.circular(12)),
 //                         ),
-//                         Icon(Icons.arrow_forward_ios)
-//                       ],
+//                         margin: const EdgeInsets.symmetric(
+//                             vertical: 16, horizontal: 24),
+//                         padding: const EdgeInsets.all(16),
+//                         child: const Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                           children: [
+//                             Expanded(
+//                               child: Row(
+//                                 children: [
+//                                   Icon(Icons.multitrack_audio),
+//                                   SizedBox(width: 16),
+//                                   Text(
+//                                     'Set up speech profile',
+//                                     style: TextStyle(
+//                                         color: Colors.white, fontSize: 16),
+//                                   ),
+//                                 ],
+//                               ),
+//                             ),
+//                             Icon(Icons.arrow_forward_ios)
+//                           ],
+//                         ),
+//                       ),
 //                     ),
-//                   ),
+//                     Positioned(
+//                       top: 12,
+//                       right: 24,
+//                       child: Container(
+//                         width: 12,
+//                         height: 12,
+//                         decoration: const BoxDecoration(
+//                             color: Colors.red, shape: BoxShape.circle),
+//                       ),
+//                     ),
+//                   ],
 //                 ),
-//                 Positioned(
-//                   top: 12,
-//                   right: 24,
-//                   child: Container(
-//                     width: 12,
-//                     height: 12,
-//                     decoration: const BoxDecoration(
-//                         color: Colors.red, shape: BoxShape.circle),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//       ..._getConnectedDeviceWidgets(),
-//       TranscriptWidget(
-//           btDevice: widget.device,
-//           key: widget.transcriptChildWidgetKey,
-//           refreshMemories: widget.refreshMemories,
-//           setHasTranscripts: (hasTranscripts) {
-//             if (_hasTranscripts == hasTranscripts) return;
-//             setState(() {
-//               _hasTranscripts = hasTranscripts;
-//             });
-//           }),
-//       const SizedBox(height: 16)
-//     ]);
+//           ..._getConnectedDeviceWidgets(),
+//           TranscriptWidget(
+//               btDevice: widget.device,
+//               key: widget.transcriptChildWidgetKey,
+//               refreshMemories: widget.refreshMemories,
+//               setHasTranscripts: (hasTranscripts) {
+//                 if (_hasTranscripts == hasTranscripts) return;
+//                 setState(() {
+//                   _hasTranscripts = hasTranscripts;
+//                 });
+//               }),
+//           const SizedBox(height: 16)
+//         ],
+//       ),
+//     );
 //   }
 
 //   _getConnectedDeviceWidgets() {
 //     if (_hasTranscripts) return [];
 //     if (widget.device == null) {
 //       return [
-//         const DeviceAnimationWidget(
+//         const ScanningAnimationWidget(
 //           sizeMultiplier: 0.7,
 //         ),
 //         const ScanningUI(
@@ -115,53 +301,68 @@
 //             string3: 'Keep it near your phone for pairing'),
 //       ];
 //     }
-//     // return [const DeviceAnimationWidget()];
 //     return [
-//       const Center(child: DeviceAnimationWidget(sizeMultiplier: 0.7)),
-//       Center(
-//           child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         mainAxisAlignment: MainAxisAlignment.center,
+//       Stack(
 //         children: [
-//           const SizedBox(width: 24),
-//           Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             mainAxisAlignment: MainAxisAlignment.start,
-//             children: [
-//               const Text(
-//                 'Listening',
-//                 style: TextStyle(
-//                     fontFamily: 'SF Pro Display',
-//                     color: Colors.white,
-//                     fontSize: 29.0,
-//                     letterSpacing: 0.0,
-//                     fontWeight: FontWeight.w700,
-//                     height: 1.2),
-//                 textAlign: TextAlign.center,
+//           Center(
+//             child: SizedBox(
+//               height: 280, // Fixed height container
+//               child: Center(
+//                 child: SineWaveWidget(sizeMultiplier: 0.7),
 //               ),
-//               Text(
-//                 'DEVICE-${widget.device?.id.split('-').last.substring(0, 6)}',
-//                 style: const TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 16.0,
-//                   fontWeight: FontWeight.w500,
-//                   height: 1.5,
-//                 ),
-//                 textAlign: TextAlign.center,
-//               )
-//             ],
+//             ),
 //           ),
-//           const SizedBox(width: 24),
-//           Container(
-//             width: 10,
-//             height: 10,
-//             decoration: const BoxDecoration(
-//               color: Color.fromARGB(255, 0, 255, 8),
-//               shape: BoxShape.circle,
+//           Positioned.fill(
+//             top: 160, // Position the content below the sine wave
+//             child: Align(
+//               alignment: Alignment.topCenter,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 mainAxisAlignment: MainAxisAlignment.start,
+//                 children: [
+//                   const SizedBox(height: 24),
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     mainAxisAlignment: MainAxisAlignment.start,
+//                     children: [
+//                       const Text(
+//                         'Listening',
+//                         style: TextStyle(
+//                             fontFamily: 'SF Pro Display',
+//                             color: Colors.white,
+//                             fontSize: 29.0,
+//                             letterSpacing: 0.0,
+//                             fontWeight: FontWeight.w700,
+//                             height: 1.2),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       Text(
+//                         'DEVICE-${widget.device?.id.split('-').last.substring(0, 6)}',
+//                         style: const TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 16.0,
+//                           fontWeight: FontWeight.w500,
+//                           height: 1.5,
+//                         ),
+//                         textAlign: TextAlign.center,
+//                       )
+//                     ],
+//                   ),
+//                   const SizedBox(height: 24),
+//                   Container(
+//                     width: 10,
+//                     height: 10,
+//                     decoration: const BoxDecoration(
+//                       color: Color.fromARGB(255, 0, 255, 8),
+//                       shape: BoxShape.circle,
+//                     ),
+//                   ),
+//                 ],
+//               ),
 //             ),
 //           ),
 //         ],
-//       )),
+//       ),
 //       const SizedBox(height: 8),
 //       const Row(
 //         crossAxisAlignment: CrossAxisAlignment.center,
@@ -172,9 +373,9 @@
 //   }
 // }
 
+import 'package:flutter/material.dart';
 import 'package:AVMe/widgets/custom_scaffold.dart';
 import 'package:AVMe/widgets/sin_wave.dart';
-import 'package:flutter/material.dart';
 import 'package:AVMe/backend/mixpanel.dart';
 import 'package:AVMe/backend/preferences.dart';
 import 'package:AVMe/backend/schema/bt_device.dart';
@@ -288,7 +489,7 @@ class _CapturePageState extends State<CapturePage>
     );
   }
 
-  _getConnectedDeviceWidgets() {
+  List<Widget> _getConnectedDeviceWidgets() {
     if (_hasTranscripts) return [];
     if (widget.device == null) {
       return [
--- apps/AppWithWearable/lib/widgets/scanning_widget.dart
+++ apps/AppWithWearable/lib/widgets/scanning_widget.dart
@@ -1,57 +1,212 @@
+// import 'package:flutter/material.dart';
+
+// class ScanningAnimationWidget extends StatefulWidget {
+//   final double sizeMultiplier;
+
+//   const ScanningAnimationWidget({super.key, this.sizeMultiplier = 1.0});
+
+//   @override
+//   State<ScanningAnimationWidget> createState() =>
+//       _ScanningAnimationWidgetState();
+// }
+
+// class _ScanningAnimationWidgetState extends State<ScanningAnimationWidget>
+//     with TickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<double> _animation;
+
+//   @override
+//   void initState() {
+//     _controller = AnimationController(
+//       duration: const Duration(milliseconds: 2000),
+//       vsync: this,
+//     )..repeat(reverse: true);
+//     _animation = Tween<double>(begin: 1, end: 0.8).animate(_controller);
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 360 * widget.sizeMultiplier,
+//       child: Center(
+//         child: Stack(
+//           alignment: Alignment.center,
+//           children: [
+//             AnimatedBuilder(
+//               animation: _animation,
+//               builder: (context, child) {
+//                 return Image.asset(
+//                   "assets/images/wave.gif",
+//                   height: 450 * widget.sizeMultiplier * _animation.value,
+//                   width: 450 * widget.sizeMultiplier * _animation.value,
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+/// You can use whatever widget as a [child], when you don't need to provide any
+/// [child], just provide an empty Container().
+/// [delay] is using a [Timer] for delaying the animation, it's zero by default.
+/// You can set [repeat] to true for making a paulsing effect.
 class ScanningAnimationWidget extends StatefulWidget {
-  final double sizeMultiplier;
+  ///initialize the ripple animation
+  const ScanningAnimationWidget({
+    required this.child,
+    this.color = Colors.black,
+    this.delay = Duration.zero,
+    this.repeat = false,
+    this.minRadius = 60,
+    this.ripplesCount = 5,
+    this.duration = const Duration(milliseconds: 2300),
+    super.key,
+  });
 
-  const ScanningAnimationWidget({super.key, this.sizeMultiplier = 1.0});
+  ///[Widget] this widget will placed at center of the animation
+  final Widget child;
+
+  ///[Duration] delay of the animation
+  final Duration delay;
+
+  /// [double] minimum radius of the animation
+  final double minRadius;
+
+  /// [Color] color of the animation
+  final Color color;
+
+  /// [int] number of circle that u want to display in the animation
+  final int ripplesCount;
+
+  /// [Duration]  of the animation
+  final Duration duration;
+
+  /// [bool] provide true if u want repeat ani9mation
+  final bool repeat;
 
   @override
-  State<ScanningAnimationWidget> createState() =>
-      _ScanningAnimationWidgetState();
+  RippleAnimationState createState() => RippleAnimationState();
 }
 
-class _ScanningAnimationWidgetState extends State<ScanningAnimationWidget>
+///state of the animation
+class RippleAnimationState extends State<ScanningAnimationWidget>
     with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+  AnimationController? _controller;
 
   @override
   void initState() {
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 2000),
+      duration: widget.duration,
       vsync: this,
-    )..repeat(reverse: true);
-    _animation = Tween<double>(begin: 1, end: 0.8).animate(_controller);
+    );
+
+    Timer? animationTimer;
+
+    animationTimer = Timer(widget.delay, () {
+      if (_controller != null && mounted) {
+        // repeating or just forwarding the animation once.
+        widget.repeat ? _controller!.repeat() : _controller!.forward();
+      }
+      // Cancel the timer when it's no longer needed.
+      animationTimer?.cancel();
+    });
+
     super.initState();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 360 * widget.sizeMultiplier,
-      child: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            AnimatedBuilder(
-              animation: _animation,
-              builder: (context, child) {
-                return Image.asset(
-                  "assets/images/wave.gif",
-                  height: 450 * widget.sizeMultiplier * _animation.value,
-                  width: 450 * widget.sizeMultiplier * _animation.value,
-                );
-              },
-            ),
-          ],
+  Widget build(BuildContext context) => CustomPaint(
+        painter: CirclePainter(
+          _controller,
+          color: widget.color,
+          minRadius: widget.minRadius,
+          wavesCount: widget.ripplesCount + 2,
         ),
-      ),
-    );
+        child: widget.child,
+      );
+}
+
+/// Creating a Circular painter for clipping the rects and creating circle shape
+class CirclePainter extends CustomPainter {
+  ///initialize the painter
+  CirclePainter(
+    this.animation, {
+    required this.wavesCount,
+    required this.color,
+    this.minRadius,
+  }) : super(repaint: animation);
+
+  ///[Color] of the painter
+  final Color color;
+
+  ///[double] minimum radius of the painter
+  final double? minRadius;
+
+  ///[int] number of wave count in the animation
+  final int wavesCount;
+
+  ///[Color] of the painter
+  final Animation<double>? animation;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = Rect.fromLTRB(0, 0, size.width, size.height);
+    for (int wave = 0; wave <= wavesCount; wave++) {
+      circle(
+        canvas,
+        rect,
+        minRadius,
+        wave,
+        animation!.value,
+        wavesCount,
+        color,
+      );
+    }
   }
+
+  /// animating the opacity according to min radius and waves count.
+  void circle(
+    Canvas canvas,
+    Rect rect,
+    double? minRadius,
+    int wave,
+    double value,
+    int? length,
+    Color circleColor,
+  ) {
+    Color color = circleColor;
+    double r;
+    if (wave != 0) {
+      final double opacity =
+          (1 - ((wave - 1) / length!) - value).clamp(0.0, 1.0);
+      color = color.withOpacity(opacity);
+
+      r = minRadius! * (1 + (wave * value)) * value;
+      final Paint paint = Paint()..color = color;
+      canvas.drawCircle(rect.center, r, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CirclePainter oldDelegate) => true;
 }
