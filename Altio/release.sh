9cb5fe5f74869975e31d021146549dd817d45082
rahulpscraftech360 <rahul@craftech360.com>
2025-02-13 19:06:44 +0530

    Green Commit : Device Based UI + Action Items UI

--- Altio/release.sh
+++ Altio/release.sh
@@ -5,9 +5,14 @@ flutter build apk --release --flavor prod -t lib/main_prod.dart
 
 
 
-
+//IOS APK
 flutter build ipa --flavor prod --release
+
+//ANDROID APK
 flutter build apk --flavor prod --release
+
+//ANDROID APP BUNDLE
+flutter build appbundle --flavor prod --release
 
 For a universal APK (single APK for all architectures):
 flutter build apk --flavor prod --release --target-platform android-arm,android-arm64,android-x64
